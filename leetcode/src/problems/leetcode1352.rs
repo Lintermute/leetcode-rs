eaaf1ac1d14561ed0966698e018f3685f5c8be38
Andreas Waidler <lintermute@mailbox.org>
2025-02-14 17:12:22 +0100

    [1352] Use prefix product algorithm

--- leetcode/src/problems/leetcode1352.rs
+++ leetcode/src/problems/leetcode1352.rs
@@ -4,20 +4,37 @@
 
 #[derive(Default)]
 pub struct ProductOfNumbers {
-    nums: Vec<i32>,
+    prods: Vec<i32>,
 }
 
 impl ProductOfNumbers {
     pub fn new() -> Self {
-        Self { nums: vec![] }
+        Self { prods: vec![] }
     }
 
     pub fn add(&mut self, num: i32) {
-        self.nums.push(num)
+        if num == 0 {
+            self.prods.clear();
+        } else {
+            let last = self.prods.last().unwrap_or(&1);
+            self.prods.push(last * num);
+        }
     }
 
     pub fn get_product(&self, k: i32) -> i32 {
         let k = usize::try_from(k).unwrap();
-        self.nums.iter().rev().take(k).product()
+        let n = self.prods.len();
+        let last = self.prods.last().copied().unwrap_or(0);
+
+        use core::cmp::Ordering::*;
+        match k.cmp(&n) {
+            Greater => 0,
+            Equal => last,
+            Less => {
+                let i = n - k - 1;
+                let prefix = self.prods[i];
+                last / prefix
+            }
+        }
     }
 }
